248b3aa7d72dcf72a11d9678eb4d751e3d0483cc
denghb <i@denghb.com>
2019-04-03 01:34:14 +0800

    init

--- simplex-service-impl/src/main/java/com/denghb/simplex/service/impl/SysUserServiceImpl.java
+++ simplex-service-impl/src/main/java/com/denghb/simplex/service/impl/SysUserServiceImpl.java
@@ -1,5 +1,16 @@
 package com.denghb.simplex.service.impl;
 
 
-public class SysUserServiceImpl {
+import com.denghb.simplex.sys.service.SysUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SysUserServiceImpl implements SysUserService {
+
+    @Override
+    public void save() {
+
+    }
 }
