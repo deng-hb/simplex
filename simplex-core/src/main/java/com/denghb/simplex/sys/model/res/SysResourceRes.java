af6fa2b0e1ea709e1a65c4e9f18cb752f4607e86
denghb <i@denghb.com>
2019-04-17 00:20:42 +0800

    update

---
+++ simplex-core/src/main/java/com/denghb/simplex/sys/model/res/SysResourceRes.java
@@ -0,0 +1,27 @@
+package com.denghb.simplex.sys.model.res;
+
+import lombok.Data;
+
+/**
+ * @Auther: denghb
+ * @Date: 2019/4/13 21:38
+ */
+@Data
+public class SysResourceRes {
+
+    private Integer id;
+
+    private Integer parentId;
+
+    private String type;
+
+    private String title;
+
+    private String path;
+
+    private String icon;
+
+    private Integer seq;
+
+    private Integer opened;
+}
