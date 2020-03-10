9c34b3b439721aa388f737b90a708b5233d446de
null <null>
2020-03-10 20:32:36 +0800

    test package

--- package.sh
+++ package.sh
@@ -3,12 +3,16 @@ mvn clean install -DskipTests=true
 
 project='simplex'
 #version=`date +%Y%m%d%H%M%S`
-version=`git tag -l`
+version=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 echo ''
 echo "build docker image $project:$version"
 
+# build
 docker build -t $project:$version .
+
+# stop
+docker stop $project
 
 # run
 docker run -p 8081:8081 --name $project -d $project:$version
